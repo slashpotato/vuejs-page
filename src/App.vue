819b2ea484a6a6c13024315007b697bb84bd5581
slashpotato <slashpotato@proton.me>
2024-09-13 14:33:39 +0400

    general commit

--- src/App.vue
+++ src/App.vue
@@ -5,38 +5,38 @@ const count = ref(0)
 </script>
 
 <template>
-    <div class="gap-2 flex items-center w-fit h-screen m-auto justify-center">
-        <span>{{ count }}</span>
-        <button @click="count--" :disabled="count <= 0">-</button>
-        <button @click="count++">+</button>
-    </div>
+  <div class="gap-2 flex items-center w-fit h-screen m-auto justify-center">
+    <span>{{ count }}</span>
+    <button @click="count--" :disabled="count <= 0">-</button>
+    <button @click="count++">+</button>
+  </div>
 </template>
 
 <style scoped>
 header {
-    line-height: 1.5;
+  line-height: 1.5;
 }
 
 .logo {
-    display: block;
-    margin: 0 auto 2rem;
+  display: block;
+  margin: 0 auto 2rem;
 }
 
 @media (min-width: 1024px) {
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
 
-    .logo {
-        margin: 0 2rem 0 0;
-    }
+  .logo {
+    margin: 0 2rem 0 0;
+  }
 
-    header .wrapper {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 }
 </style>
